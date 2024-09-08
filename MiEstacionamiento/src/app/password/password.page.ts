57883356df2ea6513e23fd3e84835fd978c82e77
KatyMiyu <katy.miyu24@gmail.com>
2024-09-08 18:46:52 -0300

    Validaciones de la page password y modificacion toolbar

--- MiEstacionamiento/src/app/password/password.page.ts
+++ MiEstacionamiento/src/app/password/password.page.ts
@@ -1,4 +1,6 @@
+import { query } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router'; //libreria que permite enviar datos enviados por paginas
 
 @Component({
   selector: 'app-password',
@@ -7,7 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordPage implements OnInit {
 
-  constructor() { }
+  name: string ='';//variable nombre usuario
+  password: string =''//variable para la contraseña
+  password2: string =''//variable para la contraseña
+  errors: string[] = [];//array para almacenar el error
+
+  constructor(private router: Router) { }
+
+  //Validar datos
+  validarDatos(){
+    this.errors = [];
+
+    //Condiciones
+    if (this.name.trim()===''){
+      this.errors.push('Nombre es obligatorio');
+    }
+    if (this.password.trim()===''){
+      this.errors.push('Contraseña obligatoria')
+    }
+    if(this.password.length<6){
+      this.errors.push('La contraseña debe tener mas de 6 carácteres')
+    }
+    if (this.password2.trim()===''){
+      this.errors.push('Contraseña obligatoria')
+    }
+    if(this.password2.length<6){
+      this.errors.push('La contraseña debe tener mas de 6 carácteres')
+    }
+
+    //Si no hay errores se guardan los datos y se regresa al login
+
+    if (this.errors.length === 0) {
+      const navigationExtras: NavigationExtras = {
+        queryParams: {
+          user: this.name, //envio dato del input usuario
+        },
+      };
+      this.router.navigate(['login'], navigationExtras); //Navega a login y envia el dato del input al Usuario
+    }
+    
+
+
+
+  }
 
   ngOnInit() {
   }
